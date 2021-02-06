cc57fcdb81f4ce3de81d390c5e9a3de7ed2b05c0
Weird_Kusou <amanwhohave@gmail.com>
2021-02-06 16:42:59 +0700

    Add files via upload

---
+++ E-Office Desktop/Form1.vb
@@ -0,0 +1,4 @@
+﻿Public Class Form1
+    Private Sub WebBrowser1_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
+    End Sub
+End Class
